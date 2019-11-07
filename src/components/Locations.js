e37635bcef4873b212e76d33afc936a2584dbd47
Adam McKnight <adammcknight@me.com>
2019-11-07 16:39:46 -0500

    Updated Times

--- src/components/Locations.js
+++ src/components/Locations.js
@@ -6,7 +6,9 @@ const Locations = () => {
     <div className="Locations">
       <div className="details">
         <h1>Times and Locations</h1>
-        <h3>Saturday, December 7th at 3pm</h3>
+        <h3>Saturday, December 7th</h3>
+        <h3>Production begins at 3pm</h3>
+        <h3>Christmas Extravaganza 5pm to 6pm</h3>
         <h4>CityGate Church Lebanon</h4>
         <p>1004 Columbus Avenue. Lebanon, Ohio 46036</p>
       </div>
