1fe56ec09fccd4f45e115acf2d665a2ecf6fe14b
Adam McKnight <adammcknight@me.com>
2019-11-08 13:17:16 -0500

    Updated Details

--- src/components/Details.js
+++ src/components/Details.js
@@ -5,7 +5,7 @@ const Details = () => {
   return (
     <div className="Details">
       <p>
-      This year CityGate Church is KICKING CHRISTMAS UP A NOTCH with Christmas UNWRAPPED! Joy - The Greatest Gift to the World! This production will explore the reasons why JOY is the greatest gift to the world. After the production we will have an awesome Christmas Extravaganza for the kids that will be from 5pm to 6pm,. that will include Hot Chocolate bars, Letters to Santa, Ornament making, Candy Canes, Gingerbread Men, and... PICTURES WITH SANTA!!! Do Not Miss this night of WONDER!
+      This year CityGate Kids is KICKING CHRISTMAS UP A NOTCH with Christmas UNWRAPPED! Joy - The Greatest Gift to the World! This production will explore the reasons why JOY is the greatest gift to the world. <br/> <br/> We are also have a CHRISTMAS EXTRAVAGANZA that will be from 5pm to 6pm. That will include Hot Chocolate bars, Letters to Santa, Ornament making, Candy Canes, Gingerbread Men, and... PICTURES WITH SANTA!!! Do Not Miss this night of WONDER!
       </p>
     </div>
   );
